17e1ae749ca207c787f6333702d2be075160790a
HoiDam <66233626+HoiDam@users.noreply.github.com>
2021-04-02 16:05:28 +0800

    added check token valid function (find user)

--- php/public/index.php
+++ php/public/index.php
@@ -4,12 +4,10 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 // ------------------------------------------------------
 
-// # include Slim framwork
-require '../vendor/autoload.php';
-// -----------------------------------------------------
-
-// -- include DB connection file ---
-require '../src/config/db.php';
+// -- include libarary--------
+require '../vendor/autoload.php'; //slim framwork
+require '../src/config/db.php'; //db
+require '/util.php'; //common util functions
 // ---------------------------------
 
 $app = new \Slim\App;
@@ -17,30 +15,25 @@ $app = new \Slim\App;
 // --- Routes -------------------------------------------------------
 $app->get('/', function (Request $req,  Response $res, $args = []) {
     $res = 'wrong method';
+
+    $token = "fd2cf386-b0cf-41c4-9f28-2963b398a51d";
+    $res = finduser($token);
     return $res;
 });
 
 $app->get('/api/test', function (Request $req,  Response $res, $args = []) {
 	$sql = "SELECT * FROM db_bitcoin.user;";
 	try {
- 
     $db = new db();
     $db = $db->connect();
-
-
     $stmt = $db->query( $sql );
-    $res = "success" ;
+    // $res = "success" ;
     $res = $stmt->fetch();
     $db = null; // clear db object
-       
-
-    
   } catch( PDOException $e ) {
-
     // show error message as Json format
     $res =  $e->getMessage();
   }
-	
     return json_encode(array("msg"=>$res));
 });
 // ------------------------------------------------------------------
